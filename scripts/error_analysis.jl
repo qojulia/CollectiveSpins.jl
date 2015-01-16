9e0d59e0e82077e0d3742c65eed97d2e9b47bb43
Sebastian Kraemer <basti.kr@gmail.com>
2015-01-16 09:13:12 +0100

    Small improvements in error analysis script.
    * Shorten collectivespins -> cs
    * Throw error when an unknown systemgeometry is given.

--- scripts/error_analysis.jl
+++ scripts/error_analysis.jl
@@ -36,16 +36,20 @@ const theta = float(parameters["theta"])
 
 # System geometry
 using quantumoptics, collectivespins
+const cs = collectivespins
+
 const edipole = float(eval(parse(parameters["edipole"])))
 const geomN = int(parameters["N"])
 const d = float(parameters["d"])
 const geomstring = parameters["geometry"]
 if geomstring=="chain"
-    const systemgeometry = collectivespins.geometry.chain(d, geomN)
+    const systemgeometry = cs.geometry.chain(d, geomN)
 elseif geomstring=="square"
-    const systemgeometry = collectivespins.geometry.square(d; Nx=geomN, Ny=geomN)
+    const systemgeometry = cs.geometry.square(d; Nx=geomN, Ny=geomN)
 elseif geomstring=="cube"
-    const systemgeometry = collectivespins.geometry.cube(d)
+    const systemgeometry = cs.geometry.cube(d)
+else
+    error("Unknown geometry: $geomstring")
 end
 
 const system = SpinCollection(systemgeometry, edipole, γ)
@@ -57,32 +61,32 @@ const td_mf = Float64[]
 const td_mpc = Float64[]
 
 # Independent
-state0 = collectivespins.independent.blochstate(phi,theta,N)
-tout, state_ind_t = collectivespins.independent.timeevolution(T, system, state0)
+state0 = cs.independent.blochstate(phi,theta,N)
+tout, state_ind_t = cs.independent.timeevolution(T, system, state0)
 
 # Meanfield
-state0 = collectivespins.meanfield.blochstate(phi,theta,N)
-tout, state_mf_t = collectivespins.meanfield.timeevolution(T, system, state0)
+state0 = cs.meanfield.blochstate(phi,theta,N)
+tout, state_mf_t = cs.meanfield.timeevolution(T, system, state0)
 
 # Meanfield + Correlations
-state0 = collectivespins.mpc.blochstate(phi,theta,N)
-tout, state_mpc_t = collectivespins.mpc.timeevolution(T, system, state0)
+state0 = cs.mpc.blochstate(phi,theta,N)
+tout, state_mpc_t = cs.mpc.timeevolution(T, system, state0)
 
 # Quantum: master equation
 
 function fout(t, rho::Operator)
     i = findfirst(T, t)
-    rho_ind = collectivespins.independent.densityoperator(state_ind_t[i])
-    rho_mf = collectivespins.meanfield.densityoperator(state_mf_t[i])
-    rho_mpc = collectivespins.mpc.densityoperator(state_mpc_t[i])
+    rho_ind = cs.independent.densityoperator(state_ind_t[i])
+    rho_mf = cs.meanfield.densityoperator(state_mf_t[i])
+    rho_mpc = cs.mpc.densityoperator(state_mpc_t[i])
     push!(td_ind, tracedistance(rho, rho_ind))
     push!(td_mf, tracedistance(rho, rho_mf))
     push!(td_mpc, tracedistance(rho, rho_mpc))
 end
 
-Ψ₀ = collectivespins.quantum.blochstate(phi,theta,N)
+Ψ₀ = cs.quantum.blochstate(phi,theta,N)
 ρ₀ = Ψ₀⊗dagger(Ψ₀)
-collectivespins.quantum.timeevolution(T, system, ρ₀, fout=fout)
+cs.quantum.timeevolution(T, system, ρ₀, fout=fout)
 
 keyparameters = Dict(
     "theta"=>parameters["theta"],
@@ -92,9 +96,9 @@ keyparameters = Dict(
 
 name = quantumoptics.io.dict2filename(keyparameters)
 f = open(joinpath(odir, name), "w")
-collectivespins.io.write_head(f, system, parameters)
-collectivespins.io.write_state(f, "timevector", T; time=false)
-collectivespins.io.write_state(f, "td_ind", td_ind; time=false)
-collectivespins.io.write_state(f, "td_mf", td_mf; time=false)
-collectivespins.io.write_state(f, "td_mpc", td_mpc; time=false)
+cs.io.write_head(f, system, parameters)
+cs.io.write_state(f, "timevector", T; time=false)
+cs.io.write_state(f, "td_ind", td_ind; time=false)
+cs.io.write_state(f, "td_mf", td_mf; time=false)
+cs.io.write_state(f, "td_mpc", td_mpc; time=false)
 close(f)
