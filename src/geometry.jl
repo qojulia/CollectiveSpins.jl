0152f83152db0852a45e16235219dab42be12e5a
Sebastian Kraemer <basti.kr@gmail.com>
2015-05-20 10:01:44 +0200

    Add triangle to geometry.

--- src/geometry.jl
+++ src/geometry.jl
@@ -3,6 +3,7 @@ module geometry
 export chain, rectangle, square, hexagonal, box, cube
 
 chain(a, N) = [i*[a,0.,0.] for i=0:N-1]
+triangle(a) = Vector{Float64}[[0.,0.,0.], [a,0.,0.], [a/2.,a*sqrt(3)/2,0]]
 rectangle(a, b; Nx=2, Ny=2) = vec([i*[a,0.,0.]+j*[0.,b,0.] for i=0:Nx-1, j=0:Ny-1])
 square(a; Nx=2, Ny=2) = vec([i*[a,0.,0.]+j*[0.,a,0.] for i=0:Nx-1, j=0:Ny-1])
 box(a, b, c; Nx=2, Ny=2, Nz=2) = vec([i*[a,0.,0.]+j*[0.,b,0.]+k*[0.,0.,c] for i=0:Nx-1, j=0:Ny-1, k=0:Nz-1])
