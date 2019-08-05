46c7dad83b02715aa1301c12470db829c9f7b380
Raphael Holzinger <raphael3d@users.noreply.github.com>
2019-08-05 15:04:18 +0200

    GreenTensor added to interaction.jl and Tests (#12)
    * GreenTensor added to interaction.jl and Tests
    
    * add test file
    
    * Docstrings and Matrix conversion for GreenTensor

--- src/interaction.jl
+++ src/interaction.jl
@@ -2,7 +2,9 @@ module interaction
 
 using ..system
 using LinearAlgebra
+import Base: *
 
+export GreenTensor,G_ij,Gamma_ij,Omega_ij
 
 """
     interaction.F(ξ, θ)
@@ -194,6 +196,103 @@ function GammaMatrix(S::system.SpinCollection)
         Γ[i,j] = interaction.Gamma(spins[i].position, spins[j].position, S.polarization, S.gamma)
     end
     return Γ
+end
+
+
+"""
+    GreenTensor(r::Vector{Float64}, k::Number)
+
+Calculate the Green's Tensor at position r for wave number k.
+The GreenTensor is a lazy type, i.e. it is not represented by a Matrix but still
+has a method implemented for Matrix-vector multiplication:
+
+    *(::GreenTensor, ::Vector{<:ComplexF64})
+
+It can be converted to a matrix as usual, Matrix(::GreenTensor).
+"""
+mutable struct GreenTensor{T<:Vector{Float64},K<:Number}
+    r::T
+    k::K
+    function GreenTensor(r::T, k::K) where {T<:Vector{Float64},K<:Number}
+        new{T,K}(r, k)
+    end
+end
+
+# Base methods for GreenTensor type
+function *(G::GreenTensor, p::Vector{T}) where T<:Number
+    k = G.k
+    R = G.r
+    n = norm(R)
+    r = R ./ n
+    exp(1.0im.*k.*n)./(4π.*n) .* ((r×p)×r .+ (1.0 ./ (k.*n).^2 .- 1.0im./(k.*n)).*(3r .* dot(r,p) .- p))
+end
+function Base.Matrix(G::GreenTensor)
+    Gmat = zeros(ComplexF64,3,3)
+    for i=1:3
+        vec = zeros(3)
+        vec[i] = 1
+Gmat[:,i] = G*vec
+    end
+    return Gmat
+end
+
+"""
+    G_ij(r1::Vector,r2::Vector,μ₁::Vector,μ₂::Vector,k0)
+
+Compute the field overlap between two atoms at positions rᵢ with dipole moments
+µᵢ, i.e.
+
+```math
+G_{ij} = µ_i ⋅ G(r_i - r_j) ⋅ µ_j.
+```
+
+It is assumed that all atoms have the same wave number k0.
+"""
+function G_ij(r1::Vector{T},r2::Vector{T},μ₁::Vector{T},μ₂::Vector{T},k0) where T<:Union{ComplexF64,Float64}
+G = GreenTensor(r1 - r2, k0)
+    k = G.k
+    R = G.r
+    n = norm(R)
+    r = R ./ n
+    return exp(1.0im.*k.*n)./(4π.*n) .* (dot(μ₁,(r×μ₂)×r) .+ (1.0 ./ (k.*n).^2 .- 1.0im./(k.*n)).*(3*dot(r,μ₁) .* dot(r,μ₂) .- dot(μ₁,μ₂)))
+end
+
+"""
+    Gamma_ij(r1::Vector,r2::Vector,μ1::Vector,μ2::Vector,k0)
+
+From the field overlap G_ij, compute the mutual decay rate as
+
+```math
+Γ_{ij} = \\frac{6π}{k0} ℑ(G_{ij}).
+```
+
+"""
+function Gamma_ij(r1::Vector{T},r2::Vector{T},μ1::Vector{T},μ2::Vector{T},k0) where T<:Union{ComplexF64,Float64}
+    if (r1 == r2) && (μ1 == μ2)
+        return 1.0
+    elseif (r1 == r2) && (μ1 != μ2)
+        return 0.0
+    else
+        return 6π/k0*imag(G_ij(r1,r2,μ1,μ2,k0))
+    end
+end
+
+"""
+    Omega_ij(r1::Vector,r2::Vector,μ1::Vector,μ2::Vector,k0)
+
+From the field overlap G_ij, compute the mutual decay rate as
+
+```math
+Ω_{ij} = -\\frac{3π}{k0} ℑ(G_{ij}).
+```
+
+"""
+function Omega_ij(r1::Vector{T},r2::Vector{T},μ1::Vector{T},μ2::Vector{T},k0) where T<:Union{ComplexF64,Float64}
+    if r1 == r2
+        return 0.0
+    else
+        return -3pi/k0*real(G_ij(r1,r2,μ1,μ2,k0))
+    end
 end
 
 end # module
